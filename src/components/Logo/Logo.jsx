6d8e4bc1445a98f40d2a7fcbdf438ab59fa5b56e
Danilo Gonzalez Ardon <danilo.j.gonzalez@gmail.com>
2022-10-10 20:05:19 -0600

    update typescript react from v.17.0.1 to 18.2.0

--- src/components/Logo/Logo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Logo = () => {
-    return (
-        <div className='logo'>/>oglez</div>
-    )
-}
-
-export default Logo
--- src/redux/actions/action-types.js
+++
@@ -1,2 +0,0 @@
-export const SELECT_LANGUAGE = "SELECT_LANGUAGE";
-export const GET_DATA = "GET_DATA";
