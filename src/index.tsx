de27939c8a67af6bf744ccbb0ec67fb03720892b
Danilo Gonzalez Ardon <danilo.j.gonzalez@gmail.com>
2022-10-10 20:41:41 -0600

    use sass

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "normalize.css";
+import "./assets/scss/styles.scss";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
