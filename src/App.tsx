09204e07c27943abdfca218dfc300f40af7a0403
Danilo Gonzalez Ardon <danilo.j.gonzalez@gmail.com>
2022-10-13 12:28:51 -0600

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import HeaderApp from "./templates/HeaderApp";
 
 function App() {
-    return <div>Hola</div>;
+    return (
+        <BrowserRouter>
+            <HeaderApp />
+            <Routes>
+                <Route path="*" element={<Navigate to="/en" replace />} />
+                <Route path="/en" element={<div>English</div>} />
+                <Route path="/es" element={<div>Español</div>} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
