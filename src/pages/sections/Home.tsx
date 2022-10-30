77e3060e8f78c0e8a2fa060018da747410d03e06
Danilo Gonzalez Ardon <danilo.j.gonzalez@gmail.com>
2022-10-29 21:22:33 -0600

    Home

--- src/pages/sections/Home.tsx
+++ src/pages/sections/Home.tsx
@@ -1,10 +1,41 @@
 import React from "react";
 import ContentHooks from "../../hooks/ContentHooks";
+import TechnologicalEarth from "../../assets/video/technologicalEarth.mp4";
+import ReactTypingEffect from "react-typing-effect";
 
 const Home = () => {
-    const textConent = ContentHooks();
-    console.log(textConent);
-    return <div>Home</div>;
+    const textContent = ContentHooks();
+    const homeText = textContent.homeText;
+
+    return (
+        <section id="home" className="home">
+            <video
+                autoPlay
+                muted
+                loop
+                src={TechnologicalEarth}
+                itemType="video/mp4"
+            >
+                Sorry, your browser doesn't support videos.
+            </video>
+            <div className="home-text wrapper">
+                <div className="home-text-1">{homeText.text1}</div>
+                <div className="home-text-2">Danilo Gonzalez</div>
+                <div className="home-text-3">
+                    {homeText.text2}
+                    <span>
+                        <ReactTypingEffect
+                            text={["Front-End", "Back-End", "Full Stack"]}
+                            eraseSpeed={300}
+                            eraseDelay={3000}
+                            typingDelay={3000}
+                        />
+                    </span>
+                    {homeText.text3}
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Home;
--- src/templates/components/LanguageSelector.tsx
+++ src/templates/components/LanguageSelector.tsx
@@ -25,13 +25,21 @@ const LanguageSelector: FC<ILanguageSelector> = ({
                 className="dropdown-menu bg-dark"
                 aria-labelledby="navbarDropdown"
             >
-                <li className="text-primary px-2">
-                    <Link to="/en" onClick={handleDrawer}>
+                <li className="px-2">
+                    <Link
+                        to="/en"
+                        onClick={handleDrawer}
+                        className="text-primary"
+                    >
                         English
                     </Link>
                 </li>
                 <li className="text-primary px-2">
-                    <Link to="/es" onClick={handleDrawer}>
+                    <Link
+                        to="/es"
+                        onClick={handleDrawer}
+                        className="text-primary"
+                    >
                         Español
                     </Link>
                 </li>
