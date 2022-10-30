44f56bf6ea1930f09d265e0b16ef0a69cd99db58
Danilo Gonzalez Ardon <danilo.j.gonzalez@gmail.com>
2022-10-29 21:16:01 -0600

    set color in logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Logo = () => {
-    return <h3 className="navbar-logo logo text-primary">/{">"}oglez</h3>;
+    return <h3 className="navbar-logo logo">/{">"}oglez</h3>;
 };
 
 export default Logo;
