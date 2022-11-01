4d5f635c8363499448d70db0742f0cd11e035011
Danilo Gonzalez Ardon <danilo.j.gonzalez@gmail.com>
2022-11-01 16:17:50 -0600

    works section

--- src/pages/sections/Works.tsx
+++ src/pages/sections/Works.tsx
@@ -1,7 +1,41 @@
 import React from "react";
+import { Parallax } from "react-parallax";
+import ContentHooks from "../../hooks/ContentHooks";
+import CleanDesktopImg from "../../assets/img/clean-desktop.jpg";
 
 const Works = () => {
-    return <div>Works</div>;
+    const textContent = ContentHooks();
+    const worksText = textContent.worksText;
+
+    return (
+        <Parallax
+            bgImage={CleanDesktopImg}
+            bgImageAlt="Clean Desktop"
+            renderLayer={(percentage) => (
+                <div
+                    style={{
+                        position: "absolute",
+                        background: `rgba(253, 126, 20, ${percentage * 1})`,
+                        left: "50%",
+                        top: "50%",
+                        width: percentage * 500,
+                        height: percentage * 500,
+                    }}
+                />
+            )}
+        >
+            <section id="works" className="pt-5">
+                <div
+                    className="container text-secondary"
+                    style={{ height: "350px", marginTop: "150px" }}
+                >
+                    <h1 className="display-5 text-center">{worksText.title}</h1>
+                    <h1 className="display-6 text-center">{worksText.text1}</h1>
+                </div>
+            </section>
+            <div style={{ height: "100px" }} />
+        </Parallax>
+    );
 };
 
 export default Works;
